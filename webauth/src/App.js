973422d8af8278455352ba8c5217afaf6ece8640
Edgar F <eflores.ef@gmail.com>
2019-06-15 01:33:20 -0700

    initialized react app

---
+++ webauth/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() { 
+    return (
+      <div className="App">
+        <h1>Welcome to Web Auth App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
