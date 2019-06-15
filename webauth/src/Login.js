f70e4d8e35ccd857359108e6cd9cbdbf520f1f57
Edgar F <eflores.ef@gmail.com>
2019-06-15 08:27:07 -0700

    created react endpoints

---
+++ webauth/src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import api from './helpers/api';
+import { withRouter } from 'react-router-dom';
+
+class Login extends React.Component {
+   state = {
+      username: '',
+      password: ''
+   }
+
+   render() {
+      return (
+         <>
+
+         </>
+      )
+   }
+}
+
+export default withRouter(Login);
---
+++ webauth/src/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import api from './helpers/api';
+
+class Users extends React.Component {
+   state = {
+      users: []
+   }
+
+   render() {
+      return (
+         <>
+
+         </>
+      )
+   }
+}
+
+export default Users;
